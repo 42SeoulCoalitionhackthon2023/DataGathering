f6f9da790ce4071ecc156b7c807ad66b8bf56545
SeungWoonSong <susong@student.42seoul.kr>
2023-03-17 19:21:14 +0900

    feat : Update Level

---
+++ UpdateLevel.rb
@@ -0,0 +1,31 @@
+require 'dotenv/load'
+require 'mysql2'
+
+db_client = Mysql2::Client.new(
+	host: ENV.fetch('ENV_HOST'),
+	username: ENV.fetch('ENV_USERNAME'),
+	password: ENV.fetch('ENV_PASSWORD'),
+	database: ENV.fetch('ENV_DATABASE')
+)
+
+def get_user_id(db_client)
+	statement2 = db_client.prepare("SELECT user_id FROM user")
+	result = statement2.execute();
+	rows = result.to_a.map(&:values)
+	return rows
+end
+
+def update_user_level(user_id, db_client)
+  # Get the average final mark for the user's corrector
+	user_id = user_id.to_s
+  result = db_client.query("SELECT AVG(final_mark) AS avg_marks FROM feedback WHERE corrector = #{user_id[1..-2]}")
+	avg_final_mark = result.first["avg_marks"].to_i
+	db_client.query("UPDATE user SET level = #{avg_final_mark} WHERE user_id = #{user_id[1..-2]}")
+end
+
+ids = get_user_id(db_client)
+total = ids.length
+for index in 0..total - 1
+	update_user_level(ids[index], db_client)
+end
+# update_user_level(["68927"], db_client)
